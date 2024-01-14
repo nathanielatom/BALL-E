66047a98fed8fc485f580d8a6e304aae9128af62
Adam Suban-Loewen <adam.subanloewen@gmail.com>
2024-01-14 14:34:47 -0800

    added temperature conversion from celsius to fahrenheit

--- source/Temp_FtoC.cpp
+++ source/Temp_FtoC.cpp
@@ -2,11 +2,19 @@
 #include <format>
 
 double Temp_FtoC(double F) {
-	// Function to convert temperature from fehrenheit to celsius
+	// Function to convert temperature from Fahrenheit to degrees Celsius
 	
 	double C = (F - 32.0) * (5.0 / 9.0);
 
 	return C;
+}
+
+double Temp_CtoF(double C) {
+	// Function to convert temperature from degrees Celsius to Fahrenheit
+
+	double F = C * 9.0 / 5.0 + 32.0;
+
+	return F;
 }
 
 int main() {
@@ -14,9 +22,25 @@ int main() {
 
 	std::string input_temp;
 	std::cin >> input_temp;
-	double F = std::stod(input_temp);
-	double C = Temp_FtoC(F);
-	std::cout << std::format("Temperature in Celsius is {} Celsius\n", C);
+
+	std::cout << "Is this in Celsius (C) or Fahrenheit (F)?";
+	std::string input_unit;
+	std::cin >> input_unit;
+
+	if (input_unit == "F") {
+		double F = std::stod(input_temp);
+		double C = Temp_FtoC(F);
+		std::cout << std::format("Temperature in degrees Celsius is {} ºC\n", C);
+	}
+	else if (input_unit == "C") {
+		double C = std::stod(input_temp);
+		double F = Temp_CtoF(C);
+		std::cout << std::format("Temperature in degrees Fahrenheit is {} ºF\n", F);
+	}
+	else {
+		std::string message = std::format("Unknown temperature unit: {}", input_unit);
+		throw std::invalid_argument(message);
+	}
 
 	return 0;
 }
