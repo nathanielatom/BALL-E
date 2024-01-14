4bc6da54e77e9224c063682de016af689e5839c6
hatfie10 <cooper.hatfield@mail.utoronto.ca>
2024-01-14 17:06:21 -0500

    Fixed countdown and temp converter

--- source/Countdown.cpp
+++ source/Countdown.cpp
@@ -3,14 +3,15 @@
 int main() {
 	// Integer countdown function which counts down to 0 from given integer
 
-	std::cin >> std::string user_input;
+	std::string user_input;
+	std::cin >> user_input;
 
 	int N = std::stoi(user_input);
 
 	for (int i = N; i > 0; i --) {
 
-		std::cout << std::itos(i);
+		std::cout << i << '\n';
 	}
 
-	return 0
+	return 0;
 }
--- source/Temp_FtoC.cpp
+++ source/Temp_FtoC.cpp
@@ -4,17 +4,19 @@
 double Temp_FtoC(double F) {
 	// Function to convert temperature from fehrenheit to celsius
 	
-	double C = (F - 32) * (5/9);
+	double C = (F - 32.0) * (5.0 / 9.0);
 
 	return C;
 }
 
-void main() {
+int main() {
 	// Print statement for Temp_FtoC
 
-	double F << std::stod(std::cin);
-
+	std::string input_temp;
+	std::cin >> input_temp;
+	double F = std::stod(input_temp);
 	double C = Temp_FtoC(F);
-
 	std::cout << std::format("Temperature in Celsius is {} Celsius\n", C);
+
+	return 0;
 }
